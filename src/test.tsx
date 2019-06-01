35eebe01c24404124839b50a6a2cc841646e75f4
Макаров Андрей <a.makarov@itech-group.ru>
2019-06-01 21:25:27 +0400

    add storyook config

--- src/test.tsx
+++ src/test.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { hot } from 'react-hot-loader/root';
+import { hot } from 'react-hot-loader/root'
 
 export interface IHelloProps { compiler: string; framework: string; }
 
