95850ff534045b9befca24584fb8e504142d492c
Макаров Андрей <a.makarov@itech-group.ru>
2019-06-01 17:46:37 +0400

    modify gitignore

--- src/test.tsx
+++ src/test.tsx
@@ -1,9 +1,9 @@
 import * as React from 'react';
 import { hot } from 'react-hot-loader/root';
 
-export interface HelloProps { compiler: string; framework: string; }
+export interface IHelloProps { compiler: string; framework: string; }
 
-const Hello = (props: HelloProps) => (
-  <div><h1>React with TS</h1></div>
+const Hello = (props: IHelloProps) => (
+  <div><h1>React with TS3422</h1></div>
 );
 export default hot(Hello);
